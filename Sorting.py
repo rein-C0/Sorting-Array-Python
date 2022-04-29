ca52eea3112b19bc1976ba0df9d96beae87ce7aa
Theo Skynet <reincomando@gmail.com>
2022-04-29 23:58:28 +0700

    Initial commit

---
+++ Sorting.py
@@ -0,0 +1,39 @@
+def SelectionSort(arr,reverse=False):
+    for i in range(len(arr)):
+        min=i
+        for j in range(i+1,len(arr)):
+            if(reverse):
+                if arr[min]<arr[j]:
+                    min=j
+            else:
+                if arr[min]>arr[j]:
+                    min=j
+        arr[i],arr[min]=arr[min],arr[i]
+    return arr
+
+def BubbleSort(arr,reverse=False):
+    for i in range(len(arr)):
+        for j in range(0,len(arr)-i-1):
+            if(reverse):
+                if arr[j]<arr[j+1]:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+            else:
+                if arr[j]>arr[j+1]:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+def InsertionSort(arr,reverse=False):
+    for i in range(1,len(arr)):
+        current=arr[i]
+        j=i-1
+        if(reverse):
+            while(j>=0 and arr[j]<current):
+                arr[j+1]=arr[j]
+                j-=1
+        else:
+            while(j>=0 and arr[j]>current):
+                arr[j+1]=arr[j]
+                j-=1
+        arr[j+1]=current
+        print(arr)
+    return arr
---
+++ main.py
@@ -0,0 +1,6 @@
+import Sorting
+
+if __name__ == '__main__':
+    randArr=[31, 25, 11, 84, 5, 101]
+    sortArr=Sorting.InsertionSort(randArr)
+    print(sortArr)
