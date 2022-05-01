96b0d5288157129d1eaa2d3c14c68259960e2987
Theo Skynet <reincomando@gmail.com>
2022-05-02 06:57:46 +0700

    update quick sort and heap sort

--- Sorting.py
+++ Sorting.py
@@ -64,3 +64,63 @@ def MergeSort(arr,reverse=False):
     if(reverse):
         arr=arr[::-1]
     return arr
+
+def Partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+    for j in range(low, high):
+        if(arr[j]<=pivot):
+            i+=1
+            arr[j],arr[i]=arr[i],arr[j]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+
+def QuickSort(arr,low,high,reverse=False):
+    if(low<high):
+        pi=Partition(arr,low,high)
+        QuickSort(arr,low,pi-1)
+        QuickSort(arr,pi+1,high)
+    if(reverse):
+        arr=arr[::-1]
+    return arr
+
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+ 
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[largest] < arr[l]:
+        largest = l
+ 
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+ 
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+ 
+        # Heapify the root.
+        heapify(arr, n, largest)
+ 
+# The main function to sort an array of given size
+ 
+ 
+def heapSort(arr,reverse=False):
+    n = len(arr)
+ 
+    # Build a maxheap.
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
+ 
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+        
+    if(reverse):
+        arr=arr[::-1]
+    return arr
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import Sorting
 
 if __name__ == '__main__':
-    randArr=[31, 25, 11, 84, 5, 101]
+    randArr=[31, 25, 11, 84, 5, 101, 33]
     sortArr=Sorting.SelectionSort(randArr)
     sortArr=Sorting.BubbleSort(randArr)
     sortArr=Sorting.InsertionSort(randArr)
     sortArr=Sorting.MergeSort(randArr)
+    sortArr=Sorting.QuickSort(randArr,0,len(randArr)-1)
+    sortArr=Sorting.heapSort(randArr)
     print(sortArr)
